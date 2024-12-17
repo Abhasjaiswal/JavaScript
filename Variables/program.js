c7a315ee5577b6aab1a12f69b38a8eb8646977d1
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-12-17 17:04:50 +0530

    updates

---
+++ Variables/program.js
@@ -0,0 +1,17 @@
+let message;
+
+message = "Hello, World!";
+
+console.log(message);
+
+// We can also declare mutiple variables in a single line
+
+let message1, age, name;
+
+message = "Hello ! Abhas Jaiswal";
+age = 21;
+name = "Abhas";
+
+console.log(message);
+console.log(age);
+console.log(name);
